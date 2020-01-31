821fe778866bfe478c1856536f3ca0181e5c2dc3
Matthew-Brown-18 <58653564+Matthew-Brown-18@users.noreply.github.com>
2020-01-31 01:30:44 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+    
+    
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Game Review Website</title>
+</head>
+<body>
+    <div class="text">
+       <h1>Game Reccomendation Website</h1> 
+    </div>
+</body>
+</html>
+
